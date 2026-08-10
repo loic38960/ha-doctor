54d47d719a50f3022342ae91d06e4080e94ec0d1
loic38960 <148827112+loic38960@users.noreply.github.com>
2026-08-10 12:10:58 +0200

    Use hardened 0.8.5 semantic wiring

--- ha_doctor/scanner_v085.py
+++ ha_doctor/scanner_v085.py
@@ -2,7 +2,7 @@
 import time
 
 import scanner_v084 as base
-import intelligence_v085
+import intelligence_v085_fixed as intelligence
 
 VERSION = "0.8.5"
 
@@ -10,7 +10,7 @@ VERSION = "0.8.5"
 def scan(include_yaml=True):
     started = time.monotonic()
     report = base.scan(include_yaml=include_yaml)
-    report = intelligence_v085.enrich_v085(report)
+    report = intelligence.enrich_v085(report)
     report["version"] = VERSION
     report["scan_duration_seconds"] = round(time.monotonic() - started, 3)
     return report
